3b9c41581335ff511ad273177aaa623eb6eca1e3
Ambuj <ext.ambuj.kumar@stadiumgoods.com>
2022-09-13 16:27:43 +0530

    TDDs1 and FE start

---
+++ foundry/src/Medium.sol
@@ -0,0 +1,53 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.9;
+
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
+
+contract Medium is ERC721, ERC721URIStorage, Ownable {
+    using Counters for Counters.Counter;
+
+    Counters.Counter private _tokenIdCounter;
+
+    uint256 public fees;
+
+    event owner_address(address);
+
+    constructor(
+        uint256 _fees
+        ) ERC721("MediumIpfs", "MI") {
+        fees = _fees;
+    }
+
+    function safeMint(address to, string memory uri) public payable {
+
+        require(msg.value >= fees, "Not Enough Eth");
+        emit owner_address(owner());
+        payable(owner()).transfer(fees);
+
+        uint256 tokenId = _tokenIdCounter.current();
+        _tokenIdCounter.increment();
+        _safeMint(to, tokenId);
+        _setTokenURI(tokenId, uri);
+
+        uint256 contractBalance = address(this).balance;
+        if (contractBalance > 0){
+            payable(msg.sender).transfer(address(this).balance);
+        }
+    }
+
+    function _burn(uint256 tokenId) internal override(ERC721, ERC721URIStorage) {
+        super._burn(tokenId);
+    }
+
+    function tokenURI(uint256 tokenId)
+        public
+        view
+        override(ERC721, ERC721URIStorage)
+        returns (string memory)
+    {
+        return super.tokenURI(tokenId);
+    }
+}
---
+++ foundry/test/Medium.t.sol
@@ -0,0 +1,25 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.9;
+
+import "forge-std/Test.sol";
+import "../src/Medium.sol";
+
+contract MediumTest is DSTest {
+    Medium public md;
+    function setUp() public {
+        md = new Medium(1 ether);
+        address addr = 0x1234567890123456789012345678901234567890;
+        md.safeMint{value: 1 ether}(addr, "ipfs://QmYgb3tawCnMGPMV4DGFawNznEBzoFNXsZNwvTPDB4mZxc");
+    }
+
+    function testSafeMint() public {
+        address addr = 0x1234567890123456789012345678901234567890;
+        address owner = md.ownerOf(0);
+        assertEq(addr, owner);
+    }
+
+    // function testSetNumber(uint256 x) public {
+    //     counter.setNumber(x);
+    //     assertEq(counter.number(), x);
+    // }
+}
