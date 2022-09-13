51de72899f2049476e88488693f89cfb34aadd98
Ambuj <ext.ambuj.kumar@stadiumgoods.com>
2022-09-13 18:34:54 +0530

    frontend changes

--- svelte/src/routes/+page.svelte
+++ svelte/src/routes/+page.svelte
@@ -1,2 +1,32 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+    import WalletConnect from "../lib/WalletConnect.svelte";
+    import Medium from "../lib/Medium.svelte"
+    import contractAbi from "../contracts/Medium.json";
+    const contractAddr =  "";
+    export let web3Props = {
+        provider : null,
+        signer : null,
+        account : null,
+        chainId : null
+    };
+</script>
+
+<div class="OUTER">
+<h1>Play with Your Pet Emoji</h1>
+</div>
+{#if !web3Props.account}
+<WalletConnect bind:web3Props {contractAddr} {contractAbi} />
+{:else}
+<Medium bind:web3Props />
+{/if}
+
+<style>
+    .OUTER {
+        border-radius: 15px;
+        padding: 20px;
+        overflow: hidden;
+        position: relative;
+        background-color: rgb(214, 226, 43);
+        box-shadow: 1px 4px 1px rgba(0,0,0,0.3);
+    }
+</style>
